4c0690a46ac3549703015712446397d7f8b3a752
@_Stahlz <joshwhitaker@hotmail.com>
2020-10-15 20:37:14 -0400

    Update bazaar.py

--- bazaar.py
+++ bazaar.py
@@ -1,4 +1,5 @@
 from datetime import date
+from pathlib import Path
 import subprocess
 import requests
 import json
@@ -6,50 +7,45 @@ import json
 
 global request, API_KEY, file_name
 request = requests.Session()
-API_KEY = ''
+API_KEY = 'bb025a4d75d99a2a02991d22fd6b5b11'
 today = date.today()
 date = today.strftime("%m-%d-%Y")
 file_name = date + '.txt'
 
 def check_if_outfile_exists():
-    is_file_there = subprocess.Popen(['ls ./output/{0}'.format(file_name)], shell=True, stdout=subprocess.PIPE)
-    is_file_there = is_file_there.stdout.read().decode('utf-8')
-    if 'cannot access' in is_file_there:
-    	touch_file = subprocess.Popen(['touch ./output/{0}'.format(file_name)], shell=True, stdout=subprocess.PIPE)
-    	print('Creating File: {0}'.format(file_name))
-
+    does_file_exist = Path("./output/{}".format(file_name))
+    if does_file_exist.is_file():
+    	pass
+    else:
+    	open('./output/{}'.format(file_name), 'a').close()
 
 def write_to_file(md5_hash):
-	try:
-		with open(file_name, 'r+') as dedup:
-			output = dedup.read().splitlines()
-		for dedup in output:
-			if md5_hash in output:
-				print('Dupe Found: {0}'.format(md5_hash))
-				continue
+	with open('./output/{}'.format(file_name), 'r+') as output:
+		hashes = output.read().splitlines()
+		#output.close()
+		for hashe in hashes:
+			print(hashe)
+			if md5_hash == hashe:
+				print('Dupe: {}'.format(md5_hash))
 			else:
-				with open(file_name, 'a+') as hashed:
-					hashed.write(md5_hash + '\n')
-	except Exception as failed_write:
-		print(failed_write)
+				with open('./output/{}'.format(file_name), 'a+') as hashes:
+					hashes.write(hashe)
 
 
 def query_bazaar():
 	url = 'https://mb-api.abuse.ch/api/v1/'
-	data = {'query': 'get_recent', 'selector': 'time'}
+	data = {'query': 'get_recent', 'selector': '100'}
 
 	query_it = request.post(url, data=data)
 	queried = query_it.json()
 	#print(json.dumps(queried, indent=4))
 	if queried['query_status'] == 'ok':
 		for each in queried['data']:
-			if each['md5_hash'] != '' or None:
-				write_to_file(each['md5_hash'])
-			else:
-				print('No New Malware')
+			write_to_file(each['md5_hash'])
 	elif queried['query_status'] == 'no_results':
 		print('No Results')
 
 check_if_outfile_exists()
 query_bazaar()
+
 
