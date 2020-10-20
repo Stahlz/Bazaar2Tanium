833442e0ae312fc36c8c5367e2a86f208846b91a
@_Stahlz <joshwhitaker@hotmail.com>
2020-10-20 16:51:17 -0400

    Update tanium.py

--- tanium.py
+++ tanium.py
@@ -1,7 +1,16 @@
 from requests import Session
+import configparser
 
-global request
+"""Configparser setup"""
+configfilepath = './config.py'
+config = configparser.ConfigParser()
+config.read(configfilepath)
+
+global request,url,username,password
 request = requests.Session()
+url = config.get('Tanium', 'url')
+username = config.get('Tanium', 'username')
+password = config.get('Tanium', 'password')
+
 
 def create_api_token():
-	
