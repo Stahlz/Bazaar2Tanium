529c99e258646f4c2e03a6ee9803a160d57b8c80
@_Stahlz <joshwhitaker@hotmail.com>
2021-05-16 14:33:36 -0400

    Update tanium.py

--- tanium.py
+++ tanium.py
@@ -1,19 +1,49 @@
-from requests import Session
+import requests
 import configparser
+import json
+import time
+import urllib3
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 """Configparser setup"""
 configfilepath = './config.py'
 config = configparser.ConfigParser()
 config.read(configfilepath)
 
-global request,url,username,password
+global request,url,username,password,domain
 request = requests.Session()
-url = config.get('Tanium', 'url')
-username = config.get('Tanium', 'username')
-password = config.get('Tanium', 'password')
+base_url = config.get('Tanium', 'url').strip("'")
+username = config.get('Tanium', 'username').strip("'")
+password = config.get('Tanium', 'password').strip("'")
+domain = ''
+
+def authentication():
+    header = {'Content-Type': 'application/octet-stream', 'cache-control': 'no-cache'}
+    data = "{\n  \"username\" : \"" + username + "\",\n    \"password\" : \"" + password + "\",\n    \"Domain\": \"" + domain + "\"\n}"
+    url = '{}/api/v2/session/login'.format(base_url)
+    get_auth_token = request.post(url, headers=header, data=data, verify=False)
+    json_token = get_auth_token.json()
+    global token
+    token = json_token['data']['session']
+
+def results_asked_question(id):
+	header = {'Content-Type': 'application/json', 'cache-control': 'no-cache', 'session': '{}'.format(token)}
+	url = '{0}/api/v2/result_data/question/{1}?include_hashes_flag=1'.format(base_url,id)
+	question_result = request.post(url, headers=header, verify=False)
+	question_result = question_result.json()
+	print(json.dumps(question_result, indent=4))
 
 
-def create_api_token():
-	data = {'username': username, 'password': password}
-	get_token = request.post(url, json=data)
-	
+def ask_question():
+	header = {'Content-Type': 'application/octet-stream', 'cache-control': 'no-cache', 'session': '{}'.format(token)}
+	url = '{}/api/v2/questions'.format(base_url)
+	question = 'Get Sample Sensor from all machines'
+	question_query = request.post(url, headers=header, json=question, verify=False)
+	question_query = question_query.json()
+	question_id = question_query['data']['id']
+	time.sleep(30)
+	results_asked_question(question_id)
+
+
+authentication()
+ask_question()
